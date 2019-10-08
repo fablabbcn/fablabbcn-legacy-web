9df4e50cf3b9050b3e8c6a28f4dffa24cf050b68
Viktor Smari <viktorsmari@gmail.com>
2019-10-08 15:39:14 +0200

    Wrap modal trigger in document ready

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-console.log('Ready')
+$( document ).ready(function() {
+  console.log('Document ready')
 
-
-// When you land on /#thanks after submitting modal
-if(window.location.hash === '#modal-thanks'){
-  $('#finishModal').modal({ });
-}
+  // When you land on /#thanks after submitting modal
+  if(window.location.hash === '#modal-thanks'){
+    $('#finishModal').modal({ });
+  }
+});
