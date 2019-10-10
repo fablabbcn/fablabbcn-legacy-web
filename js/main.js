94f699d7f02a95dd407a36382c44029d6b8ef9d5
Viktor Smari <viktorsmari@gmail.com>
2019-10-10 17:49:53 +0200

    Add lightGallery plugin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
   console.log('Document ready')
 
+  $('#video-gallery').lightGallery({
+    loadYoutubeThumbnail: true,
+    youtubeThumbSize: 'default',
+  });
+
+
   // When you land on /#thanks after submitting modal
   if(window.location.hash === '#modal-thanks'){
     $('#finishModal').modal({ });
